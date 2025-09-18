47c7bef39c6eebc7124a3f484bfd956777be04a9
AdrianTayeh <81419977+AdrianTayeh@users.noreply.github.com>
2025-09-18 11:28:20 +0200

    added env var

--- auth.ts
+++ auth.ts
@@ -16,8 +16,14 @@ const providers: Provider[] = [
       };
     },
   }),
-  GitHub,
-  Google,
+  GitHub({
+    clientId: process.env.GITHUB_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+  }),
+  Google({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  }),
 ];
 
 export const providerMap = providers
