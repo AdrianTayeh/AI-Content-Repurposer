f83e489598abec1a9bec876fbcb655e4d5ef1b79
AdrianTayeh <81419977+AdrianTayeh@users.noreply.github.com>
2025-09-23 14:55:06 +0200

    added session.user.id in auth

--- auth.ts
+++ auth.ts
@@ -53,4 +53,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     signIn: "/login",
   },
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token?.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
 });
